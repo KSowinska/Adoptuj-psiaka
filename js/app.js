af5f530961e721e0ce399a4ebd12af4f33c2ea38
Katarzyna <sowinska.k@gmail.com>
2017-03-21 22:46:28 +0100

    add _media, js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
 $(function() {
 
 	$(".info").on("mouseenter", function(){
-		console.log("działa js");
+		console.log("działa js <3");
 	});
 
 	var whatFor = $(".main").find("#whatFor");
 
 	$(".why").on("click", function(){
-		whatFor.show();
+		whatFor.toggle();
 		//console.log("dziala");
+
 
 	});
 
-
-
+	var whatShouldKnow = $(".main").find("#whatShouldKnow");
+	$(".info").on("click", function(){
+		whatShouldKnow.toggle();
+	});
 });
+
